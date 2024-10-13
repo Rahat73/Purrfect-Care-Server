11eba6e16b197c75c3a5a4f163f7871ff14a5398
Rahat73 <rahat.ashik.18@gmail.com>
2024-10-13 20:29:38 +0600

    ssl port

--- src/app/utils/send-email.ts
+++ src/app/utils/send-email.ts
@@ -4,7 +4,7 @@ import nodemailer from 'nodemailer';
 export const sendEmail = async (to: string, html: string) => {
   const transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com.',
-    port: 587,
+    port: 465,
     secure: config.NODE_ENV === 'production',
     auth: {
       // TODO: replace `user` and `pass` values from <https://forwardemail.net>
